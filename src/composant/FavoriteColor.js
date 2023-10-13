0419a2c282a01dc548f09b9646b64f6f2771dffe
smlcisse <idanisoumailacisse@gmail.com>
2023-10-13 16:59:23 +0100

    mon premier dossier

--- src/composant/FavoriteColor.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-function FavoriteColor() {
-  const [color, setColor] = useState("red");
-
-  return <h1>My favorite color is {color}!</h1>
-
-}
-export default FavoriteColor
